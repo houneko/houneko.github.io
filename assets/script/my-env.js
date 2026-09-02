589d9c7936e593c3a9445df98591794543949694
houneko <cnameany@outlook.com>
2026-09-03 08:54:11 +0900

    Add files via upload

---
+++ assets/script/my-env.js
@@ -0,0 +1,8 @@
+﻿// 環境別の設定値（グローバル変数として定義）。ぱんくずリスト用のベースフォルダ
+//  BASE_PATH     パンくずリスト用のベースパス用変数
+//  MYDEBUG       デバッグオン・オフスイッチ（trueはログ出力する）
+window.ENV_CONFIG = {
+  //BASE_PATH: "file:///C:/Users/houneko/OneDrive/My/01_MyHomePage" ,
+  BASE_PATH: "" ,
+  MYDEBUG: true
+};
